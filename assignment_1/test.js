648a8061a7f2a375b6901cb7d36841b253e0d8b6
Chris Thamo <chrisdev2016@users.noreply.github.com>
2017-06-07 23:18:03 -0400

    assignment2 complete

--- assignment_1/test.js
+++ assignment_1/test.js
@@ -5,3 +5,32 @@ var c = true;
 var result = a && b && c;
 console.log(result);
 
+var numberofreplys = 2;
+function ask(){
+    var choice = Math.floor(Math.random() * numberofreplys +1);
+    var reply = 'You need to make some options using if/else'
+
+    if(choise=1){
+ console.log(choice);
+    }
+  else if(choise>1){
+ console.log(choice);
+  }
+  else{
+ console.log(reply);
+  }
+   
+}
+
+ask();
+
+for( var i=1; i<=100; i++){
+    if(i%5 == 0){
+  console.log(fizz);
+    }
+    else if(i%3 == 0)
+      console.log(buzz);
+      else{
+    console.log(i);
+      }
+}
--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -2,9 +2,9 @@
 Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
-
-
-
+for( var i=1; i<=100; i++){
+    console.log(i);
+}
 /*
 Exercise #2
 Write a loop that makes seven calls to console.log to output the following triangle:
@@ -17,3 +17,8 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+var x= "#";
+while(x.length<8){
+    x = x + "#";
+    console.log (x);
+}
